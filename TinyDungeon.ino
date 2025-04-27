8ca538025e6ffc2d606ffd2158375769815eb045
SlickAlex <72620240+SlickAlex1@users.noreply.github.com>
2025-04-27 23:59:37 +0300

    FirstRelease

--- TinyDungeon.ino
+++ TinyDungeon.ino
@@ -10,13 +10,6 @@
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  MIT License for more details.
-
-// Resources:
-// ----------
-// A SSD1306 library for I2C is required:
-// - for ATtiny85 the <ssd1306xled> library by Tinusaur/Neven Boyanov is required.
-// - for all other micro controllers the Adafruit_SSD1306 library is used.
-//
 // Because the Arduino IDE doesn't come with ATtiny85 support, a suitable core is required:
 //   To stuff all code and data into the 8192 bytes of the ATtiny85
 //   the ATTinyCore (v1.5.2) by Spence Konde is recommended.
@@ -24,6 +17,9 @@
 //   following board manager to the Arduino IDE: [http://drazzy.com/package_drazzy.com_index.json]
 //   Please enable LTO (link time optimization) and disable 'millis()' and
 //   'micros()' and set the PLL to 16MHZ Internal PLL.
+
+//Adapted for FastTinyDriver by SlickAlex
+//This version: uses FastTinyDriver for the SSD1306 OLED; 
 
 #include "dungeon.h"
 #include "TinyJoypadUtils.h"
